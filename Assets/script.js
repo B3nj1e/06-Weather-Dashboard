cfe0cf5a523f738c007ee59f8bfc5158e1bc9362
Benjamin Clewer <benjiclewer@gmail.com>
2021-10-14 12:52:48 +1030

    local storage set up

--- Assets/script.js
+++ Assets/script.js
@@ -1,1 +1,43 @@
 console.log("test");
+
+var citySearch = document.querySelector('#city');
+var searchBtn = document.querySelector('.searchBtn');
+
+var cityCount = [];
+
+console.log(citySearch);
+console.log(searchBtn);
+
+searchBtn.addEventListener("click", function(event){
+    event.preventDefault();
+
+    console.log("search button clicked");
+    console.log(event);
+    console.log(citySearch.value)
+
+    cityCount++
+    console.log(cityCount);
+    saveCity();
+    renderCity();
+});
+
+
+
+// function searchProcess {
+    
+// };
+
+function saveCity() {
+    var cityValue = citySearch.value;
+
+    if (cityValue !== null) {
+        localStorage.setItem("city".concat(cityCount), JSON.stringify(cityValue));
+    }
+
+    console.log(cityCount);
+};
+
+function renderCity() {
+    var savedCity = JSON.parse(localStorage.getItem("city".concat(cityCount)));
+    console.log(savedCity);
+};
