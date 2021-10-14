37751cf7cf9a25f0fc38d6d66bdb5234070e1458
Benjamin Clewer <benjiclewer@gmail.com>
2021-10-14 15:04:12 +1030

    search history button, second bug fixed, duplicate buttons

--- Assets/script.js
+++ Assets/script.js
@@ -3,9 +3,8 @@ console.log("test");
 var citySearch = document.querySelector('#city');
 var searchBtn = document.querySelector('.searchBtn');
 var searchHistory = document.querySelector('.history');
-// var searchedCityBtn = document.createElement("button");
 
-var cityCount = [];
+var cityCount = JSON.parse(localStorage.getItem("cityCount"));
 
 console.log(citySearch);
 console.log(searchBtn);
@@ -20,19 +19,24 @@ searchBtn.addEventListener("click", function (event) {
     cityCount++
     console.log(cityCount);
     saveCity();
-    renderCity();
+    // renderCity();
 });
 
 // function searchProcess {
 
 // };
 
+// load previously search cities on load
+renderCity();
+
+// save searched city to local storage and to search history button
 function saveCity() {
     var cityValue = citySearch.value;
 
     if (cityValue !== null) {
         localStorage.setItem("city".concat(cityCount), JSON.stringify(cityValue));
         var searchedCityBtn = document.createElement("button");
+        localStorage.setItem("cityCount", JSON.stringify(cityCount));
         searchedCityBtn.setAttribute("type", "submit");
         searchedCityBtn.setAttribute("class", "btn btn-primary mb-3 historyBtn");
         searchedCityBtn.textContent = cityValue;
@@ -42,15 +46,17 @@ function saveCity() {
     console.log(cityCount);
 };
 
+// retrive searched cities from local storage, and display as buttons in search history
 function renderCity() {
     for (i = 1; i < 10; i++) {
         var savedCity = JSON.parse(localStorage.getItem("city".concat(i)));
         console.log(savedCity);
-        // if (savedCity) {
-        //     searchedCityBtn.setAttribute("type", "submit");
-        //     searchedCityBtn.setAttribute("class", "btn btn-primary mb-3 historyBtn");
-        //     searchedCityBtn.textContent = savedCity;
-        //     searchHistory.appendChild(searchedCityBtn);
+        if (savedCity) {
+            var searchedCityBtn = document.createElement("button");
+            searchedCityBtn.setAttribute("type", "submit");
+            searchedCityBtn.setAttribute("class", "btn btn-primary mb-3 historyBtn");
+            searchedCityBtn.textContent = savedCity;
+            searchHistory.appendChild(searchedCityBtn);
 
 
             //     console.log(searchedCityBtn);
@@ -58,9 +64,9 @@ function renderCity() {
             //     // console.log(searchedCity);
             //     // searchHistory.textContent = savedCity;
             //     // searchHistory.appendChild(searchedCity);
-        // } else {
-        //     return;
-        // }
+        } else {
+            return;
+        }
     }
     // searchHistory.appendChild("<button type='submit' class='btn btn-primary mb-3 " + savedCity + "'>" + savedCity + "</button>")
 };
