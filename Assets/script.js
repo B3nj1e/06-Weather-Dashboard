35eaf89d7a2dbfb7dda8934a7ebc050f11f1d95c
Benjamin Clewer <benjiclewer@gmail.com>
2021-10-14 14:43:44 +1030

    search history button pop up

--- Assets/script.js
+++ Assets/script.js
@@ -2,13 +2,15 @@ console.log("test");
 
 var citySearch = document.querySelector('#city');
 var searchBtn = document.querySelector('.searchBtn');
+var searchHistory = document.querySelector('.history');
+var searchedCityBtn = document.createElement("button");
 
 var cityCount = [];
 
 console.log(citySearch);
 console.log(searchBtn);
 
-searchBtn.addEventListener("click", function(event){
+searchBtn.addEventListener("click", function (event) {
     event.preventDefault();
 
     console.log("search button clicked");
@@ -21,10 +23,8 @@ searchBtn.addEventListener("click", function(event){
     renderCity();
 });
 
-
-
 // function searchProcess {
-    
+
 // };
 
 function saveCity() {
@@ -32,12 +32,34 @@ function saveCity() {
 
     if (cityValue !== null) {
         localStorage.setItem("city".concat(cityCount), JSON.stringify(cityValue));
+        searchedCityBtn.setAttribute("type", "submit");
+        searchedCityBtn.setAttribute("class", "btn btn-primary mb-3 historyBtn");
+        searchedCityBtn.textContent = cityValue;
+        searchHistory.appendChild(searchedCityBtn);
     }
 
     console.log(cityCount);
 };
 
 function renderCity() {
-    var savedCity = JSON.parse(localStorage.getItem("city".concat(cityCount)));
-    console.log(savedCity);
+    for (i = 1; i < 10; i++) {
+        var savedCity = JSON.parse(localStorage.getItem("city".concat(i)));
+        console.log(savedCity);
+        // if (savedCity) {
+        //     searchedCityBtn.setAttribute("type", "submit");
+        //     searchedCityBtn.setAttribute("class", "btn btn-primary mb-3 historyBtn");
+        //     searchedCityBtn.textContent = savedCity;
+        //     searchHistory.appendChild(searchedCityBtn);
+
+
+            //     console.log(searchedCityBtn);
+            //     // var searchedCity = document.innerHTML(savedCity);
+            //     // console.log(searchedCity);
+            //     // searchHistory.textContent = savedCity;
+            //     // searchHistory.appendChild(searchedCity);
+        // } else {
+        //     return;
+        // }
+    }
+    // searchHistory.appendChild("<button type='submit' class='btn btn-primary mb-3 " + savedCity + "'>" + savedCity + "</button>")
 };
